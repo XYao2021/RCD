f1eb571e71bf3c0481e814435ca60b4374d09590
Xin Yao <90070559+XYao2021@users.noreply.github.com>
2023-07-11 16:26:59 -0600

    Update CHOCO.py

--- CHOCO.py
+++ CHOCO.py
@@ -10,6 +10,7 @@ from partition import *
 from config import *
 from dataset.dataset import *
 from trans_matrix import *
+import time
 
 
 if device != 'cpu':
@@ -19,6 +20,8 @@ if device != 'cpu':
 if __name__ == "__main__":  #TODO: Why use this sentence
     ACC = []
     LOSS = []
+    # Seed_set = [0, 2]
+    print('Seed Set for this test: ', Seed_set)
     for seed in Seed_set:
         random.seed(seed)
         np.random.seed(seed)
@@ -52,6 +55,8 @@ if __name__ == "__main__":  #TODO: Why use this sentence
 
         Transfer = Transform(num_nodes=CLIENTS, num_neighbors=NEIGHBORS, seed=seed, network='random')
         test_model = Model(random_seed=seed, learning_rate=LEARNING_RATE, model_name=model_name, device=device, flatten_weight=True, pretrained_model_file=load_model_file)
+        print(Transfer.neighbors)
+        print(Transfer.factor)
 
         check = Check_Matrix(CLIENTS, Transfer.matrix)
         if check != 0:
@@ -68,38 +73,26 @@ if __name__ == "__main__":  #TODO: Why use this sentence
             client_accumulate.append(torch.zeros_like(model.get_weights()))
             client_residual.append(torch.zeros_like(model.get_weights()))
 
-            # client_compressor.append(Fixed_Compression(node=n, avg_comm_cost=average_comm_cost, ratio=RATIO))
-            client_compressor.append(Quantization(num_bits=4))
+            # client_compressor.append(Rand_k(node=n, avg_comm_cost=average_comm_cost, ratio=RATIO))
+            # client_compressor.append(Top_k(node=n, avg_comm_cost=average_comm_cost, ratio=RATIO))
+            client_compressor.append(Quantization(num_bits=QUANTIZE_LEVEL))
 
             client_train_loader.append(DataLoader(client_data[n], batch_size=BATCH_SIZE, shuffle=True))
             client_partition.append(Fixed_Participation(average_comp_cost=average_comp_cost))
 
         iter_num = 0
         update_times = 0
+        CONSENSUS_STEP_TMP = CONSENSUS_STEP
         # CHOCO Algorithm
         while True:  # TODO: What is the difference with for loop over clients
             print('SEED ', '|', seed, '|', 'ITERATION ', iter_num)
+            Vector_Update = []
 
             for n in range(CLIENTS):
-                model.assign_weights(weights=client_weights[n])
-                model.model.train()
+                Models[n].assign_weights(weights=client_weights[n])
+                Models[n].model.train()
 
-                # qt = client_partition[n].get_q(iter_num)
-                # if np.random.binomial(1, qt) == 1:
-                #     for i in range(ROUND_ITER):
-                #         images, labels = next(iter(client_train_loader[n]))
-                #         images, labels = images.to(device), labels.to(device)
-                #         if data_transform is not None:
-                #             images = data_transform(images)
-                #
-                #         Models[n].optimizer.zero_grad()
-                #         pred = Models[n].model(images)
-                #         loss = Models[n].loss_function(pred, labels)
-                #         loss.backward()
-                #         Models[n].optimizer.step()
-                # else:
-                #     pass
-                for i in range(ROUND_ITER):
+                for local_iter in range(ROUND_ITER):
                     images, labels = next(iter(client_train_loader[n]))
                     images, labels = images.to(device), labels.to(device)
                     if data_transform is not None:
@@ -112,17 +105,26 @@ if __name__ == "__main__":  #TODO: Why use this sentence
                     Models[n].optimizer.step()
 
                 client_tmp[n] = Models[n].get_weights()
+                # Vector_update = Models[n].get_weights()
                 Vector_update = client_tmp[n] - client_accumulate[n]
 
-                # Vector_update, _ = client_compressor[n].get_trans_bits_and_residual(w_tmp=Vector_update, w_residual=client_residual[n], iter=iter_num)
-                Vector_update, _ = client_compressor[n].get_trans_bits_and_residual(w_tmp=Vector_update, w_residual=client_residual[n], iter=iter_num)
+                Vector_update, _ = client_compressor[n].get_trans_bits_and_residual(w_tmp=Vector_update, w_residual=client_residual[n], iter=iter_num)  # Not work?
+
                 client_accumulate[n] += Vector_update
+                # Vector_Update.append(Vector_update)
 
             Averaged_accumulate = Transfer.Average_CHOCO(client_accumulate)
-            for client in range(CLIENTS):
-                client_weights[client] = client_tmp[client] + CONSENSUS_STEP * Averaged_accumulate[client]
+            # client_weights = Transfer.Average(Vector_Update)
+            for n in range(CLIENTS):
+                client_weights[n] = client_tmp[n] + CONSENSUS_STEP_TMP * Averaged_accumulate[n]
+                # client_weights[n] = client_weights[n]
 
             iter_num += 1
+            if iter_num % 200 == 0:
+                if CONSENSUS_STEP_TMP == 0.1:
+                    CONSENSUS_STEP_TMP = CONSENSUS_STEP_TMP
+                else:
+                    CONSENSUS_STEP_TMP -= 0.1
 
             # train_loss, train_acc = test_model.accuracy(weights=client_weights[0], test_loader=train_loader, device=device)
             # test_loss, test_acc = test_model.accuracy(weights=client_weights[0], test_loader=test_loader, device=device)
@@ -149,7 +151,10 @@ if __name__ == "__main__":  #TODO: Why use this sentence
 
     txt_list = [ACC, '\n', LOSS]
 
-    f = open('CHOCO|{}|{}|{}|{}.txt'.format(RATIO, ROUND_ITER, AGGREGATION, ALPHA), 'w')
+    f = open('CHOCO|{}|{}|{}|{}.txt'.format(RATIO, ROUND_ITER, AGGREGATION, time.strftime("%H:%M:%S", time.localtime())), 'w')
 
     for item in txt_list:
         f.write("%s\n" % item)
+
+    # for repeat_time in range(3):
+    #     os.system('say "Program Finished."')
